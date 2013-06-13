6cd601cce36c06517cb9fb649c37d0b138df3df7
Hein Haraldson Berg <hein@keyteq.no>
2013-06-13 13:58:37 +0200

    Fresh out of the generator, mostly.

---
+++ Controller/DefaultController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace KTQ\Bundle\KeyMediaBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    
+}
---
+++ KTQKeyMediaBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace KTQ\Bundle\KeyMediaBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class KTQKeyMediaBundle extends Bundle
+{
+}
